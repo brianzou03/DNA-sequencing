f374db00907ce4ee3d25ddc7e331aafe168cd2cd
Brian Zou <brianzou03@gmail.com>
2022-07-21 20:53:46 -0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Sequencing is the process of finding the primary structure whether it is DNA, RNA.
+
+Four bases: adenine (A), guanine (G), cytosine (C) and thymine (T)
+
+DNA sequencing is the method of determining the order of nucleotide in a DNA.
+RNA sequencing is the method to find the quantity of RNA in a biological sample.
+Protein sequencing is the method of determining the amino acid sequence of all or part of a protein or peptide.
+
+"""
+
+import Bio  # biopython
+
+# Example dataset used is: Cypripedioideae, a subfamily of orchids
+# The code can be applied to other datasets
+
