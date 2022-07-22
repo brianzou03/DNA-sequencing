f9f74892c3ef8212fefc19c504a509649e990259
Brian Zou <brianzou03@gmail.com>
2022-07-21 21:04:58 -0700

    Parsed sequences and provided relevant output

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ Protein sequencing is the method of determining the amino acid sequence of all o
 
 """
 
-import Bio  # biopython
+from Bio import SeqIO  # biopython
 
 # Example dataset used is: Cypripedioideae, a subfamily of orchids
 # The code can be applied to other datasets
 
+
+row = 0
+for sequence in SeqIO.parse('data/orchid.fasta', 'fasta'):
+    print('Sequence ID: ' + sequence.id)
+    print('DNA Sequence: ' + sequence.seq)
+    print('Number of nucleotides: {}'.format(len(sequence)) + '\n')
+    row += 1
+
+print('Total sequences in file: {}'.format(row))
