73b19a7e874eb91e42aa0ba53c8070af9ec70bff
Brian Zou <brianzou03@gmail.com>
2022-07-21 21:16:30 -0700

    added method for complementary sequences

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-"""
-Sequencing is the process of finding the primary structure whether it is DNA, RNA.
-
-Four bases: adenine (A), guanine (G), cytosine (C) and thymine (T)
-
-DNA sequencing is the method of determining the order of nucleotide in a DNA.
-RNA sequencing is the method to find the quantity of RNA in a biological sample.
-Protein sequencing is the method of determining the amino acid sequence of all or part of a protein or peptide.
-
-"""
-
 from Bio import SeqIO  # biopython
+from Bio.SeqUtils import GC
 
 # Example dataset used is: Cypripedioideae, a subfamily of orchids
 # The code can be applied to other datasets
 
 
-row = 0
-for sequence in SeqIO.parse('data/orchid.fasta', 'fasta'):
-    print('Sequence ID: ' + sequence.id)
-    print('DNA Sequence: ' + sequence.seq)
-    print('Number of nucleotides: {}'.format(len(sequence)) + '\n')
-    row += 1
+# Produces general info about the sequence to terminal
+def output_sequence_info():
+    row = 0
+    for sequence in SeqIO.parse('data/orchid.fasta', 'fasta'):
+        print('Sequence ID: ' + str(sequence.id))
+        print('DNA Sequence: ' + str(sequence.seq))
+        print('Guanine-Cytosine Content: ' + str(GC(sequence.seq)))
+        print('Number of nucleotides: {}'.format(len(sequence)) + '\n')  # DNA Sequencing
+        row += 1
 
-print('Total sequences in file: {}'.format(row))
+    print('Total sequences in file: {}'.format(row))
+
+
+# Displays complement and reverse complement of the sequence to terminal
+def dna_complement():
+    row = 1
+    for sequence in SeqIO.parse('data/orchid.fasta', 'fasta'):
+        print('Row ' + str(row))
+        print('Original Sequence:  {} '.format(sequence.seq))
+        print('Complement Sequence: {}: '.format(sequence.seq.complement()))
+        print('Reverse Complement Sequence: {}: '.format(sequence.seq.reverse_complement()) + '\n')
+        row += 1
+
+
+output_sequence_info()
+dna_complement()
